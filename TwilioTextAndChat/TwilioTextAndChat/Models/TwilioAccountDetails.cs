336113fe21aaf777531622af49b4850265495fb2
Layla Porter <layla.porter@gmail.com>
2018-06-11 10:05:06 +0100

    initial commit

---
+++ TwilioTextAndChat/TwilioTextAndChat/Models/TwilioAccountDetails.cs
@@ -0,0 +1,8 @@
+﻿namespace TwilioTextAndChat.Models
+{
+    public class TwilioAccountDetails
+    {
+        public string AccountSid { get; set; }
+        public string AuthToken { get; set; }
+    }
+}
